efbebcdea1c00e4e4c82dfb29671d296c58123db
bioid <bioshock@gmail.com>
2014-11-19 01:09:08 +0000

    add db dir creation to install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
-git submodule update --init
-cd grafana
-npm install
-npm install -g grunt-cli
-grunt
-grunt build
-cd ..
-npm install
-ln -s `pwd`/render/ `pwd`/grafana/dist/app/panels/render
-cp `pwd`/config/config.grafana.js `pwd`/grafana/dist/config.js
-cp `pwd`/config/serverconfig.sample.js `pwd`/config/serverconfig.js
-if [ ! -f `pwd`/server/projects.json ]; then
-  echo "{}" > `pwd`/server/projects.json
+# git submodule update --init
+# cd grafana
+# npm install
+# npm install -g grunt-cli
+# grunt
+# grunt build
+# cd ..
+# npm install
+# ln -s `pwd`/render/ `pwd`/grafana/dist/app/panels/render
+# cp `pwd`/config/config.grafana.js `pwd`/grafana/dist/config.js
+# cp `pwd`/config/serverconfig.sample.js `pwd`/config/serverconfig.js
+# if [ ! -f `pwd`/server/projects.json ]; then
+#   echo "{}" > `pwd`/server/projects.json
+# fi
+if [ ! -d `pwd`/db ]; then
+  mkdir db
 fi
