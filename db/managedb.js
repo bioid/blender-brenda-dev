5fbc4625d52ee5b4cc3ff05e0167311bb998a30b
bioid <bioshock@gmail.com>
2015-01-23 00:44:16 +0000

    add --createdb to managedb.js

--- db/managedb.js
+++ db/managedb.js
@@ -1,43 +1,47 @@
+#!/usr/bin/env node
 var anyDB = require('any-db'),
-    fs    = require('fs');
-
+    fs    = require('fs'),
+    argv = require('yargs').argv;
+    
 if (!global.config) {
-  global.config = require('../config/serverconfig.js') 
+  global.config = require('../config/serverconfig.js');
 }
 
-if (!fs.existsSync(global.config.projects_dir + '/projects.db')) {
-  console.log('Creating projects.db');
-  var db = anyDB.createConnection('sqlite3://' + global.config.projects_dir + '/projects.db');
-  var create_projects = 'CREATE TABLE projects' +
-                        '( project_id INTEGER PRIMARY KEY,' +
-                        'name VARCHAR(100),' + 
-                        'owner_id INT);';
-        
-  var create_jobs = 'CREATE TABLE jobs' +
-                    '( job_id INTEGER PRIMARY KEY,' +
-                    'job_name VARCHAR(100),' +
-                    'project_id INT,' +
-                    'FOREIGN KEY (project_id) REFERENCES projects(project_id));';
-  
-  db.query(create_projects, function(err, res) {
-    if (err) { console.log(err); }
-    console.log(res);
-  });
-  db.query(create_jobs, function(err, res) {
-    if (err) { console.log(err); }
-    console.log(res);
-  });
-}
-
-else {
-  console.log('projects.db already exists');
-  var db = anyDB.createConnection('sqlite3://' + global.config.projects_dir + '/projects.db');
-  db.query('SELECT * FROM projects;', function(err, res) {
-    if (err) { console.log(err); }
-    console.log(res);
-  });
-  db.query('SELECT * FROM jobs;', function(err, res) {
-    if (err) { console.log(err); }
-    console.log(res);
-  });
+if (argv.createdb) {
+  if (!fs.existsSync(global.config.projects_dir + '/projects.db')) {
+    console.log('Creating projects.db');
+    var db = anyDB.createConnection('sqlite3://' + global.config.projects_dir + '/projects.db');
+    var create_projects = 'CREATE TABLE projects' +
+                          '( project_id INTEGER PRIMARY KEY,' +
+                          'name VARCHAR(100),' + 
+                          'owner_id INT);';
+          
+    var create_jobs = 'CREATE TABLE jobs' +
+                      '( job_id INTEGER PRIMARY KEY,' +
+                      'job_name VARCHAR(100),' +
+                      'project_id INT,' +
+                      'FOREIGN KEY (project_id) REFERENCES projects(project_id));';
+    
+    db.query(create_projects, function(err, res) {
+      if (err) { console.log(err); }
+      console.log(res);
+    });
+    
+    db.query(create_jobs, function(err, res) {
+      if (err) { console.log(err); }
+      console.log(res);
+    });
+  }
+  else {
+    console.log('projects.db already exists');
+    var db = anyDB.createConnection('sqlite3://' + global.config.projects_dir + '/projects.db');
+    db.query('SELECT * FROM projects;', function(err, res) {
+      if (err) { console.log(err); }
+      console.log(res);
+    });
+    db.query('SELECT * FROM jobs;', function(err, res) {
+      if (err) { console.log(err); }
+      console.log(res);
+    });
+  }
 }
