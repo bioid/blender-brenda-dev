96e6ef6688b73cf115ae0815716b9f13e9c58e63
bioid <bioshock@gmail.com>
2015-02-12 19:28:48 +0000

    remove grafana build steps from install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
-git submodule update --init
-cd grafana
-npm install
-npm install -g grunt-cli
-grunt
-grunt build
-cd ..
 npm install
 ln -s `pwd`/render/ `pwd`/grafana/dist/app/panels/render
 cp `pwd`/config/config.grafana.js `pwd`/grafana/dist/config.js
