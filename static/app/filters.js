bef05228674a8b7bdd44836218ebd7bb2be0ffec
bioid <bioshock@gmail.com>
2015-02-13 19:57:14 +0000

    break up the angular app's factories, filters, and providers into separate files

---
+++ static/app/filters.js
@@ -0,0 +1,9 @@
+angular.module('render').filter("toArray", function(){
+  return function(obj) {
+    var result = [];
+    angular.forEach(obj, function(val, key) {
+      result.push(val);
+    });
+    return result;
+  };
+});
